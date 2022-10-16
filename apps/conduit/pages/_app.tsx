37bc259617e518509958ef2e9492edc254e90fbb
Rafael Thayto <rafa.thayto@gmail.com>
2022-10-16 18:29:51 -0300

    feat(conduit): adding nextjs layout initial config on _app.tsx

--- apps/conduit/pages/_app.tsx
+++ apps/conduit/pages/_app.tsx
@@ -1,9 +1,21 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import './styles.css'
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return getLayout(
     <>
       <Head>
         <title>Welcome to conduit!</title>
@@ -11,8 +23,8 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
-  );
+    </>,
+  )
 }
 
-export default CustomApp;
+export default CustomApp
