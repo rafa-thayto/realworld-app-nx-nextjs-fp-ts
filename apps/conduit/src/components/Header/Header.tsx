7f11320bbd8ac3d5c47fd49cf2e97da68017b2c0
Rafael Thayto <rafa.thayto@gmail.com>
2022-10-16 21:08:24 -0300

    fix(conduit): adjusting Links and styles on Header component

--- apps/conduit/src/components/Header/Header.tsx
+++ apps/conduit/src/components/Header/Header.tsx
@@ -1,35 +1,41 @@
+import Link from 'next/link'
+
 export const Header = () => {
   return (
     <nav className="navbar navbar-light">
       <div className="container">
-        <a className="navbar-brand" href="index.html">
-          conduit
-        </a>
+        <Link href="/" passHref>
+          <a className="navbar-brand">conduit</a>
+        </Link>
         <ul className="nav navbar-nav pull-xs-right">
           <li className="nav-item">
-            <a className="nav-link active" href="">
-              Home
-            </a>
+            <Link href="/home" passHref>
+              <a className="nav-link active">Home</a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
-              <i className="ion-compose"></i>&nbsp;New Article
-            </a>
+            <Link href="/articles/new" passHref>
+              <a className="nav-link">
+                <i className="ion-compose"></i>&nbsp;New Article
+              </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
-              <i className="ion-gear-a"></i>&nbsp;Settings
-            </a>
+            <Link href="/settings" passHref>
+              <a className="nav-link">
+                <i className="ion-gear-a"></i>&nbsp;Settings
+              </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
-              Sign in
-            </a>
+            <Link href="/sign-in" passHref>
+              <a className="nav-link">Sign in</a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="">
-              Sign up
-            </a>
+            <Link href="/sign-up" passHref>
+              <a className="nav-link">Sign up</a>
+            </Link>
           </li>
         </ul>
       </div>
